e86e06cdead52714602761025112d842f60e7675
ddamaris <yzu@mail.ru>
2024-06-17 23:41:46 +0300

    scala3

--- src/main/scala/homework.scala
+++ src/main/scala/homework.scala
@@ -1,61 +1,61 @@
+import MyMath.Logarithm
 
-
-
-
-
-/*
 object homework1 {
-  extension (x: String)
-    def + ?????
-
     @main def part1Ex(): Unit ={
-      println("1" + "33")
+      extension (x: String)
+        def +(y: String): String = x.concat(y)
+      val res = "1" + "33"
+      println(res)
     }
 }
 
 object homework2 {
   enum CompletionArg {
-    ???
-    //см приведенную ссылку
+    private case ShowItIsString(s: String)
+    private case ShowItIsInt(i: Int)
+    private case ShowItIsFloat(f: Float)
   }
 
-  object CompletionArg {
-    given fromString: Conversion[String, CompletionArg] = ???
+  private object CompletionArg {
+    given Conversion[String, CompletionArg] = ShowItIsString(_)
+    given Conversion[Int, CompletionArg] = ShowItIsInt(_)
+    given Conversion[Float, CompletionArg] = ShowItIsFloat(_)
 
-    given fromInt: Conversion[Int, CompletionArg] = ???
-
-    given fromFloat: Conversion[Float, CompletionArg] = ???
+    def complete[T](arg: CompletionArg): Any = arg match
+      case ShowItIsString(s) => s
+      case ShowItIsInt(i) => i
+      case ShowItIsFloat(f) => f
   }
+
   import CompletionArg.*
 
   @main def part2Ex(): Unit ={
-    println(Completions.complete("String"))
-    println(Completions.complete(1))
-    println(Completions.complete(7f))
+    println(CompletionArg.complete("String"))
+    println(CompletionArg.complete(1))
+    println(CompletionArg.complete(7f))
   }
 }
-
 
 object homework3 {
   opaque type Logarithm = Double
 
-  object Logarithm{
-    //см приведенную ссылку
+  object Logarithm {
+    def apply(d: Double): Logarithm = math.log(d)
+
+    extension (x: Logarithm)
+      def toDouble: Double = math.exp(x)
+      def +(y: Logarithm): Logarithm = Logarithm(math.exp(x) + math.exp(y))
+      def *(y: Logarithm): Logarithm = x + y
   }
-
-  extension (x: Logarithm)
-    def toDouble: ???
-    def + (y: Logarithm): ???
-    def * (y: Logarithm): ???
-
-
+  
   @main def part3Ex(): Unit ={
-    import Logarithm
+    import homework3.Logarithm
 
     val l = Logarithm(1.0)
     val l2 = Logarithm(2.0)
     val l3 = l * l2
     val l4 = l + l2
+    println(l4)
 
   }
-}*/
+}
