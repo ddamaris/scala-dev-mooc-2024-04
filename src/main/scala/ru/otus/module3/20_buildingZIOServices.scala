73d53bae918cab291ae1abfc66b2cfac7a38402a
ddamaris <yzu@mail.ru>
2024-08-22 14:46:31 +0300

    ZIO homework

--- src/main/scala/ru/otus/module3/20_buildingZIOServices.scala
+++ src/main/scala/ru/otus/module3/20_buildingZIOServices.scala
@@ -3,7 +3,7 @@ package ru.otus.module3
 import ru.otus.module3.emailService.EmailService
 import ru.otus.module3.userDAO.UserDAO
 import ru.otus.module3.userService.{UserID, UserService}
-import zio.RIO
+import zio.{RIO, ZIO}
 import zio.console.Console
 
 
@@ -14,6 +14,8 @@ object buildingZIOServices{
     UserService.notifyUser(UserID(10))
 
   lazy val env = UserDAO.live >>> UserService.live ++ EmailService.live
+
+  lazy val appRun: ZIO[Console, Throwable, Unit] = app.provideSomeLayer[Console](env)
 
   def main(args: Array[String]): Unit = {
     zio.Runtime.default.unsafeRun(app.provideSomeLayer[Console](env))
